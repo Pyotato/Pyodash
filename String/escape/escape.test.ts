764f8f60009f2f270688445cc014b3226d544ad7
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-05 15:44:48 +0900

    feat: add time log for escape test

--- String/escape/escape.test.ts
+++ String/escape/escape.test.ts
@@ -3,38 +3,45 @@ import testWithTimeLog from '../../utils/testWithTimeLog';
 import escape from './escape';
 
 testWithTimeLog(
-  `1. [escape] Escape an HTML tag : _.escape('<div>hello</div>')는 "&lt;div&gt;hello&lt;/div&gt;"다.`,
+  `1. [escape] Escape an HTML tag : escape('<div>hello</div>')는 "&lt;div&gt;hello&lt;/div&gt;"다.`,
   expect(escape('<div>hello</div>')).toBe('&lt;div&gt;hello&lt;/div&gt;')
 );
 
 testWithTimeLog(
-  `2. [escape] Escape special characters : _.escape('5 > 3 & 4 < 6')는 "5 &gt; 3 &amp; 4 &lt; 6"다.`,
+  `2. [escape] Escape special characters : escape('5 > 3 & 4 < 6')는 "5 &gt; 3 &amp; 4 &lt; 6"다.`,
   expect(escape(`5 > 3 & 4 < 6`)).toBe(`5 &gt; 3 &amp; 4 &lt; 6`)
 );
 
 testWithTimeLog(
-  `3. [escape] Escape double quotes : _.escape('"Hello"')는 "&quot;Hello&quot;"다.`,
+  `3. [escape] Escape double quotes : escape('"Hello"')는 "&quot;Hello&quot;"다.`,
   expect(escape('"Hello"')).toBe(`&quot;Hello&quot;`)
 );
 testWithTimeLog(
-  `4. [escape] Escape single quote : _.escape("'hello'")는 "&#39;hello&#39;"다.`,
+  `4. [escape] Escape single quote : escape("'hello'")는 "&#39;hello&#39;"다.`,
   expect(escape("'hello'")).toBe(`&#39;hello&#39;`)
 );
 testWithTimeLog(
-  `5. [escape] Edge Case: No characters to escape : _.escape("'hello'")는 "&#39;hello&#39;"다.`,
+  `5. [escape] Edge Case: No characters to escape : escape("'hello'")는 "&#39;hello&#39;"다.`,
   expect(escape("'hello'")).toBe(`&#39;hello&#39;`)
 );
 
 testWithTimeLog(
-  `6. [escape] Edge Case, Only symbols that don't require escaping : _.escape('')는 ""다.`,
+  `6. [escape] Edge Case, Only symbols that don't require escaping : escape('')는 ""다.`,
   expect(escape(`#@!$`)).toBe(`#@!$`)
 );
-testWithTimeLog(`7. [escape] Edge Case: Empty string : _.escape('')는 ''다.`, expect(escape(``)).toBe(``));
+testWithTimeLog(`7. [escape] Edge Case: Empty string : escape('')는 ''다.`, expect(escape(``)).toBe(``));
 testWithTimeLog(
-  `8. [escape] Edge Case: String with only space : _.escape('   ')는 "   "다.`,
+  `8. [escape] Edge Case: String with only space : escape('   ')는 "   "다.`,
   expect(escape('   ')).toBe(`   `)
 );
 
 /**
-
+  1. [escape] Escape an HTML tag : escape('<div>hello</div>')는 "&lt;div&gt;hello&lt;/div&gt;"다.: 0.041ms
+  2. [escape] Escape special characters : escape('5 > 3 & 4 < 6')는 "5 &gt; 3 &amp; 4 &lt; 6"다.: 0.002ms
+  3. [escape] Escape double quotes : escape('"Hello"')는 "&quot;Hello&quot;"다.: 0.001ms
+  4. [escape] Escape single quote : escape("'hello'")는 "&#39;hello&#39;"다.: 0.001ms
+  5. [escape] Edge Case: No characters to escape : escape("'hello'")는 "&#39;hello&#39;"다.: 0.004ms
+  6. [escape] Edge Case, Only symbols that don't require escaping : escape('')는 ""다.: 0.002ms
+  7. [escape] Edge Case: Empty string : escape('')는 ''다.: 0.001ms
+  8. [escape] Edge Case: String with only space : escape('   ')는 "   "다.: 0.001ms
  */
